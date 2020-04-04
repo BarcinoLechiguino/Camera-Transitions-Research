be08a3afc01ccc339a4bfe315576d97696d147ac
BarcinoLechuguino <angotov@gmail.com>
2020-04-04 17:35:14 +0200

    Reverted the changes made to get a clear screenshot

--- exercises/solution/Motor2D/TransitionManager.h
+++ exercises/solution/Motor2D/TransitionManager.h
@@ -26,16 +26,16 @@ public:
 	Transition* CreateFadeToColour(SCENES next_scene, float step_duration = 1.0f, Color fade_colour = Black);
 
 	Transition* CreateSlide(SCENES next_scene, float step_duration = 0.5f, bool non_lerp = false, bool vertical = false,
-									bool slide_from_right = false, bool slide_from_bottom = false, Color slide_colour = Black);
+		bool slide_from_right = false, bool slide_from_bottom = false, Color slide_colour = Black);
 
 	Transition* CreateWipe(SCENES next_scene, float step_duration = 0.5f, bool non_lerp = false, bool vertical = false,
-									bool wipe_from_right = false, bool wipe_from_bottom = false, Color wipe_colour = Black);
+		bool wipe_from_right = false, bool wipe_from_bottom = false, Color wipe_colour = Black);
 
 	Transition* CreateAlternatingBars(SCENES next_scene, float step_duration = 0.5f, bool non_lerp = false, int bar_number = 10, bool vertical = false,
-										bool random_colours = false, Color even_colour = Black, Color odd_colour = Black);
+		bool random_colours = false, Color even_colour = Black, Color odd_colour = Black);
 
 	Transition* CreateExpandingBars(SCENES next_scene, float step_duration = 0.5f, bool non_lerp = false, int bar_number = 10, bool vertical = false,
-										bool random_colours = false, Color even_colour = Black, Color odd_colour = Black);
+		bool random_colours = false, Color even_colour = Black, Color odd_colour = Black);
 
 	Transition* CreateZoomToMouse(SCENES next_scene, iPoint mouse_position, float step_duration = 0.5, bool non_lerp = false, float zoom_scale = 1.0f);
 	Transition* CreateCameraToMouse(iPoint mouse_pos, float step_duration = 0.5f, bool non_lerp = false);
